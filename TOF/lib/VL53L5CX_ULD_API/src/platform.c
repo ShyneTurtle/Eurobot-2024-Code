32dfc6379e0898443da1400c95a70d7ff4923215
frangippp <vincent5dlb@gmail.com>
2023-10-05 20:07:56 +0200

    Ajout du code en lien avec le capteur de TOF
    Contient l'environnement de développement sous Platformio pour un fonctionnement avec un rpi pico (SDK & bare metal). Contient un programme de test pour le capteur ToF VL53L5cx (dossier des versions). Le programme principale permettant une reconnaissance d'un pot de 5 cm est en développement.

---
+++ TOF/lib/VL53L5CX_ULD_API/src/platform.c
@@ -0,0 +1,151 @@
+/**
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+
+
+#include "platform.h"
+
+//Communication I2C :
+// Ecriture :
+// 	- write {index[15-8], index[7-0], data1[7-0], data2[7-0], ...}
+// Lecture :
+// 	- write {index[15-8], index[7-0]}
+// 	- read	{data1[7-0], data2[7-0], ...}
+
+uint8_t RdByte(
+		VL53L5CX_Platform *p_platform,
+		uint16_t registerAddress,
+		uint8_t *p_value)
+{
+	int status = 255;
+
+	//Conversion de l'index en 2x 8bits
+	uint8_t regAddress[2] = {(uint8_t)(registerAddress >> 8), (uint8_t)(registerAddress & 0x00ff)};
+
+	//Lecture
+	i2c_read_blocking(p_platform->i2c, p_platform->address, regAddress, 2, true);
+	status = i2c_write_blocking(p_platform->i2c, p_platform->address, p_value, 1, false);
+
+	if (status != PICO_ERROR_GENERIC)		status = 0;
+
+	return (uint8_t)status;
+}
+
+uint8_t WrByte(
+		VL53L5CX_Platform *p_platform,
+		uint16_t registerAddress,
+		uint8_t value)
+{
+	int status = 255;
+
+	// Conversion registre 8bits + value
+	uint8_t data[3] = { (uint8_t)(registerAddress >> 8), (uint8_t)(registerAddress & 0x00ff), value };
+	
+	//Tente d'écrire un octet
+	status = i2c_write_blocking(p_platform->i2c, p_platform->address, data, 3, false);
+
+	if (status != PICO_ERROR_GENERIC)		status = 0;
+
+	return (uint8_t)status;
+}
+
+uint8_t WrMulti(
+		VL53L5CX_Platform *p_platform,
+		uint16_t registerAddress,
+		uint8_t *p_values,
+		uint32_t size)
+{
+	int status = 255;
+
+	// Conversion registre 8bits + donnée1 + donnée2 + ...
+	uint8_t *data = (uint8_t*)malloc((size + 2) * sizeof(uint8_t));	
+	data[0] = (uint8_t)(registerAddress >> 8);
+	data[1] = (uint8_t)(registerAddress & 0x00ff);
+ 	for (int i = 2; i < size + 2; i++)		data[i] = p_values[i];
+
+	//Tente d'écrire plusieurs octets
+	status = i2c_write_blocking(p_platform->i2c, p_platform->address, data, size + 2, false);
+
+	if (status != PICO_ERROR_GENERIC)		status = 0;
+
+	return (uint8_t)status;
+}
+
+uint8_t RdMulti(
+		VL53L5CX_Platform *p_platform,
+		uint16_t registerAddress,
+		uint8_t *p_values,
+		uint32_t size)
+{
+	uint8_t status = 255;
+	
+	uint8_t regAddress[2] = { (uint8_t)(registerAddress >> 8), (uint8_t)(registerAddress & 0x00ff) };
+	p_values = (uint8_t*)malloc(size * sizeof(uint8_t));
+
+	//Lecture
+	i2c_read_blocking(p_platform->i2c, p_platform->address, regAddress, 2, true);
+	i2c_write_blocking(p_platform->i2c, p_platform->address, p_values, size, false);
+	/* Need to be implemented by customer. This function returns 0 if OK */
+	
+	return status;
+}
+
+uint8_t Reset_Sensor(
+		VL53L5CX_Platform *p_platform)
+{
+	uint8_t status = 0;
+	
+	/* (Optional) Need to be implemented by customer. This function returns 0 if OK */
+	
+	/* Set pin LPN to LOW */
+	/* Set pin AVDD to LOW */
+	/* Set pin VDDIO  to LOW */
+	WaitMs(p_platform, 100);
+
+	/* Set pin LPN of to HIGH */
+	/* Set pin AVDD of to HIGH */
+	/* Set pin VDDIO of  to HIGH */
+	WaitMs(p_platform, 100);
+
+	return status;
+}
+
+void SwapBuffer(
+		uint8_t 		*buffer,
+		uint16_t 	 	 size)
+{
+	uint32_t i, tmp;
+	
+	/* Example of possible implementation using <string.h> */
+	for(i = 0; i < size; i = i + 4) 
+	{
+		tmp = (
+		  buffer[i]<<24)
+		|(buffer[i+1]<<16)
+		|(buffer[i+2]<<8)
+		|(buffer[i+3]);
+		
+		memcpy(&(buffer[i]), &tmp, 4);
+	}
+}	
+
+uint8_t WaitMs(
+		VL53L5CX_Platform *p_platform,
+		uint32_t TimeMs)
+{
+	uint8_t status = 255;
+
+	sleep_ms(TimeMs);
+	status = 0;
+
+	return status;
+}
---
+++ TOF/src/main.cpp
@@ -0,0 +1,107 @@
+#include <pico/stdlib.h>
+#include <stdio.h>
+#include <hardware/gpio.h>
+#include <pico/binary_info.h>
+#include <hardware/i2c.h>
+#include "vl53l5cx_api.hpp"
+#include "dataProcessing.hpp"
+
+#define PIN_SDA 4
+#define PIN_SCL 5
+
+#define PIN_BUTTON  2
+
+//pull up de 10k sur l'I2C
+
+
+int main()  {
+
+  stdio_init_all();
+
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  gpio_init(PIN_BUTTON);
+  gpio_set_dir(PIN_BUTTON, GPIO_IN);
+
+  VL53L5CX_Configuration dev;
+  VL53L5CX_ResultsData results;
+  uint8_t rangingMode = VL53L5CX_RANGING_MODE_CONTINUOUS;
+  uint8_t status = 0;
+  uint8_t ready = 0;
+  uint32_t freq = 0;
+  uint8_t resolution = 16;
+  dataProcessing dataPro(&results, true);
+
+  dev.platform.address = 0x29;
+
+  // === I2C init ===
+  i2c_inst_t* i2c = i2c_default;
+  freq = i2c_init(i2c_default, 400000);
+  gpio_set_function(PIN_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(PIN_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(PIN_SCL);
+  gpio_pull_up(PIN_SDA);
+
+  dev.platform.i2c = i2c;
+
+  sleep_ms(3500);
+
+  printf("Setup begin...\n");
+  printf("i2c speed : %ld\n", freq);
+
+  // === VL53L5cx init ===
+
+  //Test reconnaissance
+  status = vl53l5cx_is_alive(&dev, &ready);
+  if (status == 0)    printf("VL détecté sur l'adresse %x ; r:%d\n", dev.platform.address, ready);
+  else            printf("VL non détecté, e:%d, r:%d\n", status, ready);
+
+  //Init
+  status = vl53l5cx_init(&dev); //le module doit être sous tension, LPn à 1
+  if (status == 0)    printf("init OK\n");
+  else            printf("init FAILED, e:%d\n", status);
+  
+  //Fréquence
+  status = vl53l5cx_set_ranging_frequency_hz(&dev, 60);
+  if (status == 0)    printf("set frequency OK\n");
+  else            printf("set frequency FAILED, e:%d\n", status);
+
+  //Range mode
+  status = vl53l5cx_set_ranging_mode(&dev, VL53L5CX_RANGING_MODE_CONTINUOUS);
+  if (status == 0)    printf("set range OK\n");
+  else            printf("set range FAILED, e:%d\n", status);
+
+  //4x4
+  status = vl53l5cx_set_resolution(&dev, VL53L5CX_RESOLUTION_8X8);
+  if (status == 0)    printf("set 4x4 OK\n");
+  else            printf("set 4x4 FAILED, e:%d\n", status);
+
+  printf("\nSetup done\n\n");
+  printf("start ranging...\n");
+  status = vl53l5cx_start_ranging(&dev);
+  if (status == 0)  printf(" -->DONE\n");
+  else          printf(" -->FAILED, e:%d\n", status);
+
+  gpio_put(PICO_DEFAULT_LED_PIN, true);
+
+  // === LOOP ===
+
+  while (1) {
+    
+    if (gpio_get(PIN_BUTTON)) {
+
+      status = vl53l5cx_check_data_ready(&dev, &ready);
+      if ((!status) || ready)  {
+        vl53l5cx_get_ranging_data(&dev, &results);
+        printf("=======\n");
+        dataPro.queryData();
+      }
+      else    printf("data not ready\n");
+    }
+
+    else    printf("waiting...\n");
+    sleep_ms(300);
+  }
+  return 0;
+}
