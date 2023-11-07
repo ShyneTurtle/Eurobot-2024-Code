316ac7620915677fed9ae3968e1d6e38bc7308f6
frangippp <vincent5dlb@gmail.com>
2023-11-07 17:25:42 +0100

    Prépa nouveau sdk

--- MCU Moteur/extra_script.py
+++
@@ -1,9 +0,0 @@
-env = DefaultEnvironment()
-
-def before_upload(source, target, env):
-    print("before_upload")
-    
-    # env.Execute("platformio device monitor -b 1200 -p /dev/ttyACM0")
-
-
-env.AddPreAction("upload", before_upload)
