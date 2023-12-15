c505a93b883cff8b6edf5d7087ff14c2dcca5385
ShyneTurtle <41578217+ShyneTurtle@users.noreply.github.com>
2023-12-15 16:26:51 +0100

    Slight refactor
    Fix the motor number pins

--- MCU Mecha/src/main.cpp
+++ MCU Mecha/src/main.cpp
@@ -69,20 +69,34 @@ byte* grabber_reg = i2c_reg;
 #define PLATFORM_SPEED 255
 #define PLATFORM1_BOTTOM_PIN 32
 #define PLATFORM1_TOP_PIN 25
-#define PLATFORM1_MF_PIN 2
-#define PLATFORM1_MR_PIN 4
+#define PLATFORM1_MF_PIN 16
+#define PLATFORM1_MR_PIN 12
 #define PLATFORM1_BOTTOM_BIT 0
 #define PLATFORM1_TOP_BIT 1
 #define PLATFORM1_TARGET_BIT 2
 #define PLATFORM2_BOTTOM_PIN 17
 #define PLATFORM2_TOP_PIN 18
-#define PLATFORM2_MF_PIN 16
-#define PLATFORM2_MR_PIN 12
+#define PLATFORM2_MF_PIN 2
+#define PLATFORM2_MR_PIN 4
 #define PLATFORM2_BOTTOM_BIT 3
 #define PLATFORM2_TOP_BIT 4
 #define PLATFORM2_TARGET_BIT 5
 byte* platform_reg = i2c_reg + 2;
 
+
+bool getBit(uint8_t* reg, const uint8_t bitpos) {
+    return (*reg) & (1 << bitpos);
+}
+/**
+ * @brief Utility function to work with bit flags in registers
+ * @param reg The register to edit
+ * @param bitpos The bit position
+ * @param val The new bit value
+ */
+void setBit(uint8_t* reg, const uint8_t bitpos, const bool val) {
+    (*reg) &= ~(1 << bitpos);
+    (*reg) |= val << bitpos;
+}
 
 void setup() {
     Wire.begin(0x27);
@@ -112,9 +126,7 @@ void loop() {
     for (byte bit = 0; bit < 6; bit++) {
         // === Barriers ===
         // Set to 0 the current bit
-        (*barrier_reg) &= ~(1 << bit);
-        // Set to 1 the current bit if something is detected
-        (*barrier_reg) |= (!digitalRead(barrier_pins[bit])) << bit;
+        setBit(barrier_reg, bit, !digitalRead(barrier_pins[bit]));
 
         // === Grabbers ===
         // Get the target angle based on wether the grabber should be opened or closed
@@ -126,46 +138,44 @@ void loop() {
     // Read limit switches
     bool platform1_bottom = !digitalRead(PLATFORM1_BOTTOM_PIN);
     bool platform1_top = !digitalRead(PLATFORM1_TOP_PIN);
-    // Read target from I2C reg
+    // Save limit switches values in the I2C register
+    setBit(platform_reg, PLATFORM1_BOTTOM_BIT, platform1_bottom);
+    setBit(platform_reg, PLATFORM1_TOP_BIT, platform1_top);
+
+    // Read the target position from I2C reg
     bool platform1_target = (*platform_reg) & (1 << PLATFORM1_TARGET_BIT);
     // Make the motor move to the target position
     // Set the H Bridge mode on the correct pin depending on the rotation direction
     int platform1_mode_pin = platform1_target ? PLATFORM1_MR_PIN : PLATFORM1_MF_PIN;
     digitalWrite(platform1_mode_pin, 0);
-    // Send a PWM to the H Bridge
+    // Send a PWM to the H Bridge on the correct pin depending on the rotation direction
     int platform1_pwm_pin = platform1_target ? PLATFORM1_MF_PIN : PLATFORM1_MR_PIN;
     analogWrite(
         platform1_pwm_pin,
         (platform1_target ? !platform1_top : !platform1_bottom) * PLATFORM_SPEED
     );
-    // Save reads in the I2C register
-    (*platform_reg) &= ~(1 << PLATFORM1_BOTTOM_BIT);
-    (*platform_reg) |= platform1_bottom << PLATFORM1_BOTTOM_BIT;
-    (*platform_reg) &= ~(1 << PLATFORM1_TOP_BIT);
-    (*platform_reg) |= platform1_top << PLATFORM1_TOP_BIT;
 
 
     // === Platform2 elevation ===
     // Read limit switches
     bool platform2_bottom = !digitalRead(PLATFORM2_BOTTOM_PIN);
     bool platform2_top = !digitalRead(PLATFORM2_TOP_PIN);
+    // Save limit switches values in the I2C register
+    setBit(platform_reg, PLATFORM2_BOTTOM_BIT, platform2_bottom);
+    setBit(platform_reg, PLATFORM2_TOP_BIT, platform2_top);
+
     // Read target from I2C reg
     bool platform2_target = (*platform_reg) & (1 << PLATFORM2_TARGET_BIT);
     // Make the motor move to the target position
     // Set the H Bridge mode on the correct pin depending on the rotation direction
     int platform2_mode_pin = platform2_target ? PLATFORM2_MR_PIN : PLATFORM2_MF_PIN;
     digitalWrite(platform2_mode_pin, 1);
-    // Send a PWM to the H Bridge
+    // Send a PWM to the H Bridge on the correct pin depending on the rotation direction
     int platform2_pwm_pin = platform2_target ? PLATFORM2_MF_PIN : PLATFORM2_MR_PIN;
     analogWrite(
         platform2_pwm_pin,
         (platform2_target ? !platform2_top : !platform2_bottom) * PLATFORM_SPEED
     );
-    // Save reads in the I2C register
-    (*platform_reg) &= ~(1 << PLATFORM2_BOTTOM_BIT);
-    (*platform_reg) |= platform2_bottom << PLATFORM2_BOTTOM_BIT;
-    (*platform_reg) &= ~(1 << PLATFORM2_TOP_BIT);
-    (*platform_reg) |= platform2_top << PLATFORM2_TOP_BIT;
 
     Serial.printf("Pinces: %x, Barrieres: %x, Platformes: %x\n", i2c_reg[0], i2c_reg[1], i2c_reg[2]);
 }
