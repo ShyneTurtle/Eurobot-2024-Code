a6b8651526df14cb2472d00b664f3a2bf6951028
Zodarck <justin.dupety@gmail.com>
2023-12-21 11:34:26 +0100

    Ajout du programme d'autocalibration et amélioration de la précision

--- MCU UWB (Recepteur)/src/main.cpp
+++ MCU UWB (Recepteur)/src/main.cpp
@@ -1,123 +1,316 @@
-#include <string>
-#include <map>
-#include <vector>
+#include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
 #include "DW1000Ranging.h"
+#include "DW1000.h"
+#include <string>
+#include <map>
+#include <vector>
 
 #define MOY_COUNT 10
 std::map<uint16_t, std::vector<double>> ranges;
-void rangePush(DW1000Device *device)
+void rangePush(DW1000Device *device) // place last distance measure on a 100 values string on increasing order
 {
-  ranges[device->getShortAddress()].push_back(device->getRange());
-  if (ranges.size() > MOY_COUNT)
-    ranges.erase(ranges.begin());
+    ranges[device->getShortAddress()].push_back(device->getRange());
+    if (ranges.size() > MOY_COUNT)
+        ranges.erase(ranges.begin());
 }
-double rangeMed(DW1000Device *device)
+double rangeMed(DW1000Device *device) // Calcule of mediane of 100 last distances
 {
-  std::vector<double> sorted_list = ranges[device->getShortAddress()];
-  std::sort(sorted_list.begin(), sorted_list.end());
+    std::vector<double> sorted_list = ranges[device->getShortAddress()];
+    std::sort(sorted_list.begin(), sorted_list.end());
 
-  if (sorted_list.size() % 2)
-    return (sorted_list[sorted_list.size() / 2] + sorted_list[(sorted_list.size() / 2) - 1]) / 2;
+    if (sorted_list.size() % 2)
+        return (sorted_list[sorted_list.size() / 2] + sorted_list[(sorted_list.size() / 2) - 1]) / 2;
 
-  return sorted_list[sorted_list.size() / 2];
+    return sorted_list[sorted_list.size() / 2]; // Return mediane of 100 last distances
 }
 
-void dischargedBattery();
-void veryDischargedBattery();
+// #define DEBUG_TRILAT // Prints in trilateration code
+// #define DEBUG_DIST // Print anchor distances
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 5
+
+// Connection pins
+const uint8_t PIN_RST = 14; // Reset pin
+const uint8_t PIN_IRQ = 15; // Irq pin
+const uint8_t PIN_SS = 5;   // SPI select pin
+
+// TAG antenna delay defaults to 16384
+// Leftmost two bytes below will become the "short address"
+char tag_addr[] = "7D:00:22:EA:82:60:3B:9C";
+
+// Variables for position determination
+#define N_ANCHORS 3
+#define ANCHOR_DISTANCE_EXPIRED 5000 // Measurements older than this are ignore (milliseconds)
+
+// Global variables, input and output
+
+float anchor_matrix[N_ANCHORS][3] = {
+    // List of anchor coordinates, relative to chosen origin.
+    // First column : x, Second : Y and Third : Z
+    {0.0, 0.0, 0}, // Anchor labeled #1
+    {2.0, 0.0, 0}, // Anchor labeled #2
+    {1.0, 3.0, 0}, // Anchor labeled #3
+};                 // Z values are ignored in this code
+
+uint32_t last_anchor_update[N_ANCHORS] = {0};  // Millis() value last time anchor was seen
+float last_anchor_distance[N_ANCHORS] = {0.0}; // Most recent distance reports
+
+float current_tag_position[2] = {0.0, 0.0}; // Global current position (meters with respect to anchor origin)
+float current_distance_rmse = 0.0;          // RMS error in distance calc => crude measure of position error (meters).  Needs to be better characterized
+
+// void veryDischargedBattery();
+// void veryDischargedBattery();
 void newRange();
 void newDevice(DW1000Device *device);
 void inactiveDevice(DW1000Device *device);
+int trilat2D_3A();
+void i2cReceive(int count);
+void i2cRequest();
 
-#define Diviseur_tension 33
-#define LED 13
+// Pin for the battery
+// #define Diviseur_tension 33
+// #define LED 13
+// unsigned long int lastMillis;
+
+byte i2c_reg[256] = {0};
+byte i2c_target = 0;
 
 void setup()
 {
-  Serial.begin(115200);
+    Serial.begin(115200);
+    delay(1000);
 
-  SPI.begin(18, 19, 23, 5);
+    // Set up I2C
+    Wire.begin(0x11);
+    Wire.onRequest(i2cRequest);
+    Wire.onReceive(i2cReceive);
 
-  // init the configuration
-  DW1000Ranging.initCommunication(14, 5, 15); // Reset, CS, IRQ pin
-  // define the sketch as anchor. It will be great to dynamically change the type of module
-  DW1000Ranging.attachNewRange(newRange);
-  DW1000Ranging.attachNewDevice(newDevice);
-  DW1000Ranging.attachInactiveDevice(inactiveDevice);
-  // Enable the filter to smooth the distance
-  DW1000Ranging.useRangeFilter(false);
+    // Initialize configuration
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); // Reset, CS, IRQ pin
 
-  // we start the module as a tag
-  //  DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_SHORTDATA_FAST_ACCURACY, false);
-  DW1000Ranging.startAsTag("02:00:00:00:00:00:00:01", DW1000.MODE_LONGDATA_RANGE_ACCURACY, 0);
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachNewDevice(newDevice);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+    // Start as tag, do not assign random short address
+    DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+
+    // dischargedBattery();
+    // veryDischargedBattery();
 }
 
 void loop()
 {
-  DW1000Ranging.loop();
+    DW1000Ranging.loop();
 
-  /*
-  Stockage des medianes dans un tableau :
-  Ligne 1 : module UWB qui a pour adresse 0
-  Ligne 2 : module UWB qui a pour adresse 1
-  Ligne 3 : module UWB qui a pour adresse 2
-   */
+    float x = current_tag_position[0] * 100;
+    float y = current_tag_position[1] * 100;
 
-  float TabVal1[3];
-  float x, y;
-  char addr = DW1000Ranging.getDistantDevice()->getShortAddress();
-
-  Serial.printf("%d \n", addr);
-
-  TabVal1[addr] = rangeMed(DW1000Ranging.getDistantDevice());
-
-  trilateration(TabVal1, &x, &y);
-  Serial.printf("Coordonnée en x : %f et coordonnée en y : %f\n", x, y);
-
-  dischargedBattery();
-  veryDischargedBattery();
+    i2c_reg[0] = (unsigned char)x;
+    i2c_reg[1] = ((unsigned short)y) >> 8;
+    i2c_reg[2] = ((unsigned short)y);
 }
 
-//====Fonctions====
-
-void dischargedBattery()
-{
-  if ((float)analogRead(Diviseur_tension) / 1024.00 * 3.30 <= 3.05) // Allume Led si tension batterie inférieur à 12.3V
-    digitalWrite(LED, HIGH);
-}
-void veryDischargedBattery()
-{
-  if ((float)analogRead(Diviseur_tension) / 1024.00 * 3.30 < 2.98)
-  { // Fait clignoter la Led si tension batterie inférieur à 12V
-    while (analogRead(Diviseur_tension) < 4.8)
-    {
-      digitalWrite(LED, HIGH);
-      digitalWrite(LED, LOW);
-    }
-  }
-}
+// void dischargedBattery()
+// {
+//     if ((float)analogRead(Diviseur_tension) / 1024.00 * 3.30 <= 3.05) // Led ON if Vbat<12.3V
+//         digitalWrite(LED, HIGH);
+// }
+// void veryDischargedBattery()
+// {
+//     if ((float)analogRead(Diviseur_tension) / 1024.00 * 3.30 < 2.98)
+//     { // Led blink if Vbat < 12V
+//         if ((millis() >> 9) % 2)
+//         { // Change Led state every 512ms
+//             digitalWrite(LED, !digitalRead(LED));
+//         }
+//     }
+// }
+// Collect distance data from anchors, presently configured for 4 anchors
+// Solve for position if all four current
 void newRange()
 {
-  // Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-  // Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
-  // Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
-  rangePush(DW1000Ranging.getDistantDevice());
-  Serial.printf("Mediane: %f", rangeMed(DW1000Ranging.getDistantDevice()));
-}
-void newDevice(DW1000Device *device)
+    rangePush(DW1000Ranging.getDistantDevice());
+
+    int i; // Indices, expecting values 1 to 4
+    // Index of this anchor, expecting values 1,2,3
+    int index = DW1000Ranging.getDistantDevice()->getShortAddress() & 0x03;
+
+    // Find the good antenna delay
+    if (index > 0)
+    {
+        last_anchor_update[index - 1] = millis();                 // Decrement for array index
+        float range = rangeMed(DW1000Ranging.getDistantDevice()); // DW1000Ranging.getDistantDevice()->getRange();
+        last_anchor_distance[index - 1] = range;
+        if (range < 0.0 || range > 30.0)
+            last_anchor_update[index - 1] = 0; // Sanity check, ignore this measurement
+    }
+
+    int detected = 0;
+
+    // Reject old measurements
+    for (i = 0; i < N_ANCHORS; i++)
+    {
+        if (millis() - last_anchor_update[i] > ANCHOR_DISTANCE_EXPIRED)
+            last_anchor_update[i] = 0; // Not from this one
+        if (last_anchor_update[i] > 0)
+            detected++;
+    }
+
+    if (detected == 3)
+    { // Three measurements TODO: check millis() wrap
+
+#ifdef DEBUG_DIST
+      // Print distance and age of measurement
+        uint32_t current_time = millis();
+        for (i = 0; i < N_ANCHORS; i++)
+        {
+            Serial.print(last_anchor_distance[i]);
+            Serial.print("\t");
+            Serial.println(current_time - last_anchor_update[i]); // Age in millis
+        }
+#endif
+
+        trilat2D_3A();
+
+        // Output the values (X, Y and error estimate)
+        Serial.print("P = ");
+        Serial.print(current_tag_position[0]);
+        Serial.write(',');
+        Serial.print(current_tag_position[1]);
+        Serial.write(',');
+        Serial.println(current_distance_rmse);
+    }
+} // End newRange
+void newDevice(DW1000Device *device) // Print address of new devices
 {
-  Serial.print("ranging init; 1 device added ! -> ");
-  Serial.print(" short:");
-  Serial.println(device->getShortAddress(), HEX);
+    Serial.print("Device added: ");
+    Serial.println(device->getShortAddress(), HEX);
 }
-void inactiveDevice(DW1000Device *device)
+void inactiveDevice(DW1000Device *device) // Print address of inactive device
 {
-  Serial.print("delete inactive device: ");
-  Serial.println(device->getShortAddress(), HEX);
+    Serial.print("Delete inactive device: ");
+    Serial.println(device->getShortAddress(), HEX);
 }
-void trilateration(float r[], float *x, float *y)
+int trilat2D_3A(void)
 {
-  *x = (r[0] * r[0] - r[1] * r[1] + 40000) / 400;
-  *y = (r[0] * r[0] - r[2] * r[2] + 100000 - 200 * *x) / 600;
+    /*
+    For method see technical paper at
+    https://www.th-luebeck.de/fileadmin/media_cosa/Dateien/Veroeffentlichungen/Sammlung/TR-2-2015-least-sqaures-with-ToA.pdf
+
+    A nice feature of this method is that the normal matrix depends only on the anchor arrangement
+    and needs to be inverted only once. Hence, the position calculation should be robust.
+    */
+    static bool first = true;         // First time through, some preliminary work
+    float b[N_ANCHORS], d[N_ANCHORS]; // Temp vector, distances from anchors
+
+    static float Ainv[2][2], k[N_ANCHORS]; // These are calculated only once
+
+    int i;
+    // Copy distances to local storage
+    for (i = 0; i < N_ANCHORS; i++)
+        d[i] = last_anchor_distance[i];
+
+#ifdef DEBUG_TRILAT
+    char line[60];
+    snprintf(line, sizeof line, "d: %6.2f %6.2f %6.2f", d[0], d[1], d[2]);
+    Serial.println(line);
+#endif
+
+    if (first)
+    { // Intermediate fixed vectors
+        first = false;
+
+        float x[N_ANCHORS], y[N_ANCHORS]; // Intermediate vectors
+        float A[2][2];                    // The A matrix for system of equations to solve
+
+        for (i = 0; i < N_ANCHORS; i++)
+        {
+            x[i] = anchor_matrix[i][0];
+            y[i] = anchor_matrix[i][1];
+            k[i] = x[i] * x[i] + y[i] * y[i];
+        }
+
+        // Set up least squares equation
+
+        for (i = 1; i < N_ANCHORS; i++)
+        {
+            A[i - 1][0] = x[i] - x[0];
+            A[i - 1][1] = y[i] - y[0];
+#ifdef DEBUG_TRILAT
+            snprintf(line, sizeof line, "A  %5.2f %5.2f \n", A[i - 1][0], A[i - 1][1]);
+            Serial.println(line);
+#endif
+        }
+        // Invert A
+        float det = A[0][0] * A[1][1] - A[1][0] * A[0][1];
+        if (fabs(det) < 1.0E-4)
+        {
+            Serial.println("***Singular matrix, check anchor coordinates***");
+            while (1)
+                delay(1); // Hang
+        }
+
+#ifdef DEBUG_TRILAT
+        snprintf(line, sizeof line, "det A %8.3e\n", det);
+        Serial.println(line);
+#endif
+
+        det = 1.0 / det;
+        // Scale adjoint
+        Ainv[0][0] = det * A[1][1];
+        Ainv[0][1] = -det * A[0][1];
+        Ainv[1][0] = -det * A[1][0];
+        Ainv[1][1] = det * A[0][0];
+    } // End if (first);
+
+    for (i = 1; i < N_ANCHORS; i++)
+    {
+        b[i - 1] = d[0] * d[0] - d[i] * d[i] + k[i] - k[0];
+    }
+
+    // Least squares solution for position
+    // Solve:  2 A rc = b
+
+    current_tag_position[0] = 0.5 * (Ainv[0][0] * b[0] + Ainv[0][1] * b[1]);
+    current_tag_position[1] = 0.5 * (Ainv[1][0] * b[0] + Ainv[1][1] * b[1]);
+
+    // Calculate RMS error for distances
+    float rmse = 0.0, dc0 = 0.0, dc1 = 0.0;
+    for (i = 0; i < N_ANCHORS; i++)
+    {
+        dc0 = current_tag_position[0] - anchor_matrix[i][0];
+        dc1 = current_tag_position[1] - anchor_matrix[i][1];
+        dc0 = d[i] - sqrt(dc0 * dc0 + dc1 * dc1);
+        rmse += dc0 * dc0;
+    }
+    current_distance_rmse = sqrt(rmse / ((float)N_ANCHORS));
+
+    return 1;
+} // End trilat2D_3A
+void i2cReceive(int count)
+{
+    bool reg_addr_byte = true;
+    while (Wire.available())
+    {
+        if (reg_addr_byte)
+        {
+            i2c_target = Wire.read();
+        }
+        else
+        {
+            i2c_reg[i2c_target++] = Wire.read();
+        }
+        reg_addr_byte = false;
+    }
+}
+void i2cRequest()
+{
+    Wire.write(i2c_reg[i2c_target++]);
 }
--- MCU UWB (Source)/src/main.cpp
+++ MCU UWB (Source)/src/main.cpp
@@ -1,56 +1,151 @@
+// This program calibrates an ESP32_UWB module intended for use as a fixed anchor point
+// uses binary search to find anchor antenna delay to calibrate against a known distance
+//
+// modified version of Thomas Trojer's DW1000 library is required!
+
+// Remote tag (at origin) must be set up with default antenna delay (library default = 16384)
+
+// user input required, possibly unique to each tag:
+// 1) accurately measured distance from anchor to tag
+// 2) address of anchor
+//
+// output: antenna delay parameter for use in final anchor setup.
+
 #include <SPI.h>
 #include "DW1000Ranging.h"
+#include "DW1000.h"
 
-void newRange();
-void newBlink(DW1000Device *device);
-void inactiveDevice(DW1000Device *device);
+// ESP32_UWB pin definitions
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 5
 
 // connection pins
 const uint8_t PIN_RST = 14; // reset pin
 const uint8_t PIN_IRQ = 15; // irq pin
-const uint8_t PIN_SS = 5; // spi select pin
+const uint8_t PIN_SS = 5;   // spi select pin
 
+// char this_anchor_addr[] = "02:00:00:00:00:00:00:01";
+// char this_anchor_addr[] = "02:00:00:00:00:00:00:02";
+char this_anchor_addr[] = "02:00:00:00:00:00:00:03";
+float this_anchor_target_distance = 1; // measured distance to anchor in m
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
+uint16_t this_anchor_Adelay = 16600; // starting value
+uint16_t Adelay_delta = 100;         // initial binary search step size
 
-  SPI.begin(18, 19, 23, 5);
+void veryDischargedBattery();
+void veryDischargedBattery();
+void newRange();
+void newDevice(DW1000Device *device);
+void inactiveDevice(DW1000Device *device);
 
-  //init the configuration
-  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
-  //define the sketch as anchor. It will be great to dynamically change the type of module
-  DW1000Ranging.attachNewRange(newRange);
-  DW1000Ranging.attachBlinkDevice(newBlink);
-  DW1000Ranging.attachInactiveDevice(inactiveDevice);
-  //Enable the filter to smooth the distance
-  DW1000Ranging.useRangeFilter(true);
-  
-  //we start the module as an anchor
-  //DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_SHORTDATA_FAST_ACCURACY, false);
-  DW1000Ranging.startAsAnchor("02:00:00:00:00:00:00:03", DW1000.MODE_LONGDATA_RANGE_ACCURACY, 0);
-  
+// For the battery
+#define Diviseur_tension 33
+#define LED 13
+unsigned long int lastMillis;
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial)
+        ;
+    // init the configuration
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); // Reset, CS, IRQ pin
+
+    Serial.print("Starting Adelay ");
+    Serial.println(this_anchor_Adelay);
+    Serial.print("Measured distance ");
+    Serial.println(this_anchor_target_distance);
+
+    DW1000.setAntennaDelay(this_anchor_Adelay);
+
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachNewDevice(newDevice);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+    // Enable the filter to smooth the distance
+    // DW1000Ranging.useRangeFilter(true);
+
+    // start the module as anchor, don't assign random short address
+    DW1000Ranging.startAsAnchor(this_anchor_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
 }
 
-void loop() {
-  DW1000Ranging.loop(); 
+void loop()
+{
+    DW1000Ranging.loop();
 
+    // dischargedBattery();
+    // veryDischargedBattery();
 }
 
-void newRange() {
-  Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
-  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+void dischargedBattery()
+{
+    if ((float)analogRead(Diviseur_tension) / 1024.00 * 3.30 <= 3.05) // Led ON if Vbat<12.3V
+        digitalWrite(LED, HIGH);
+}
+void veryDischargedBattery()
+{
+    if ((float)analogRead(Diviseur_tension) / 1024.00 * 3.30 < 2.98)
+    { // Led blink when Vbat < 12V
+        if ((millis() >> 9) % 2)
+        { // Change Led state every 512ms
+            digitalWrite(LED, !digitalRead(LED));
+        }
+    }
+}
+void newRange()
+{
+    static float last_delta = 0.0;
+    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), DEC);
+
+    float dist = DW1000Ranging.getDistantDevice()->getRange();
+
+    Serial.print(",");
+    Serial.print(dist);
+    if (Adelay_delta < 3)
+    {
+        Serial.print(", final Adelay ");
+        Serial.println(this_anchor_Adelay);
+        //    Serial.print("Check: stored Adelay = ");
+        //    Serial.println(DW1000.getAntennaDelay());
+        // while (1)
+        // {
+        //     Serial.print("from: ");
+        //     Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+        //     // Serial.print("\n");
+        //     Serial.print("\t Range: ");
+        //     Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+        //     Serial.println(" m");
+        // }; // done calibrating
+    }
+
+    float this_delta = dist - this_anchor_target_distance; // error in measured distance
+
+    if (this_delta * last_delta < 0.0)
+        Adelay_delta = Adelay_delta / 2; // sign changed, reduce step size
+    last_delta = this_delta;
+
+    if (this_delta > 0.0)
+        this_anchor_Adelay += Adelay_delta; // new trial Adelay
+    else
+        this_anchor_Adelay -= Adelay_delta;
+
+    Serial.print(", Adelay = ");
+    Serial.println(this_anchor_Adelay);
+    //  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+    DW1000.setAntennaDelay(this_anchor_Adelay);
 }
 
-void newBlink(DW1000Device* device) {
-  Serial.print("blink; 1 device added ! -> ");
-  Serial.print(" short:");
-  Serial.println(device->getShortAddress(), HEX);
+void newDevice(DW1000Device *device)
+{
+    Serial.print("Device added: ");
+    Serial.println(device->getShortAddress(), HEX);
 }
 
-void inactiveDevice(DW1000Device* device) {
-  Serial.print("delete inactive device: ");
-  Serial.println(device->getShortAddress(), HEX);
+void inactiveDevice(DW1000Device *device)
+{
+    Serial.print("delete inactive device: ");
+    Serial.println(device->getShortAddress(), HEX);
 }
-
